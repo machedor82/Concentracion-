3d5dd953987a1aca5740571e3dc91fb5084e6f57
machedor82 <A01177924@tec.mx>
2025-06-09 18:11:31 -0600

    Update Cabrito_Dash.py

--- Cabrito_Dash.py
+++ Cabrito_Dash.py
@@ -241,4 +241,4 @@ with tabs[2]:
     meses = {
         1:"Enero",2:"Febrero",3:"Marzo",4:"Abril",5:"Mayo",6:"Junio",
         7:"Julio",8:"Agosto",9:"Septiembre",10:"Octubre",
-        11:"Noviembre"
+        11:"Noviembre"}
